f779e50e2b65fb66c7b4a42cb3a81c3b14d9cb3b
kurban_b <bdobik@mail.ru>
2023-06-30 12:37:38 +0300

    сверстана страница 404-ой и 500-ой ошибок

---
+++ packages/client/src/views/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,39 @@
+import { FC, useMemo } from 'react'
+import { NavLink } from 'react-router-dom'
+import style from './errorPage.module.css'
+
+export interface ErrorPageType {
+  status?: number
+}
+
+/** Страница с статусом ошибки 404/500 */
+const ErrorPage: FC<ErrorPageType> = props => {
+  const { status = 500 } = props
+
+  const message = useMemo(() => {
+    switch (status) {
+      case 404:
+        return 'Страница не найдена'
+      default:
+        return 'Ошибка при загрузке страницы'
+    }
+  }, [status])
+
+  return (
+    <div className={style.block}>
+      <div color={style.wrapper}>
+        <h1 className={style.statusText}>{status}</h1>
+
+        <h2 className={style.message}>{message}</h2>
+
+        <div className={style.buttonBlock}>
+          <NavLink to={'/'} className={style.button}>
+            Вернутся на главную
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ErrorPage

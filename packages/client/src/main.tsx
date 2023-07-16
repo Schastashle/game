76ed51db2749772f13d8535d3079176c231a5607
mrdarkside <tomitomion@gmail.com>
2023-07-16 13:05:36 +0300

    :sparkles: добавляет ErrorBoundary

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store'
 import App from './App'
+import { ErrorBoundary } from './components'
 
 import './index.css'
 
@@ -11,7 +12,9 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <ErrorBoundary>
+          <App />
+        </ErrorBoundary>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>

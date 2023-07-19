761e03b03488ee84b2f3f3a39d949067202994ea
Mikael Tadesse <Mikael.Tadesse@miningelement.com>
2023-07-19 16:25:53 +0300

    добавил кнопку и метод для переключения режима просмотра

--- packages/client/src/views/Game/Game.tsx
+++ packages/client/src/views/Game/Game.tsx
@@ -9,6 +9,8 @@ export default class Game extends Component {
   private readonly canvas: HTMLCanvasElement
   private readonly canvasRef: React.RefObject<HTMLDivElement>
   private gameAPI: GameAPI
+  private fullscreenButtonText: string
+  private isFullscreenMode: boolean
 
   constructor(props: object) {
     super(props)
@@ -26,10 +28,13 @@ export default class Game extends Component {
 
     this.gameAPI.drawGameGrid()
     this.gameAPI.distributeGems()
+
+    this.fullscreenButtonText = 'На весь экран'
+    this.isFullscreenMode = false
   }
 
   componentDidMount(): void {
-    ;(this.canvasRef.current as HTMLDivElement).appendChild(this.canvas)
+    ;(this.canvasRef.current as HTMLDivElement).prepend(this.canvas)
   }
 
   onSelectGem = (event: React.MouseEvent<HTMLDivElement>) => {
@@ -40,12 +45,27 @@ export default class Game extends Component {
     this.gameAPI.setSelectedGem(x, y)
   }
 
+  toggleFullscreen = () => {
+    this.isFullscreenMode
+      ? document.exitFullscreen()
+      : (this.canvasRef.current as HTMLDivElement).requestFullscreen()
+    this.isFullscreenMode = !this.isFullscreenMode
+  }
+
   render() {
     return (
       <div
         className={styles.game}
         ref={this.canvasRef}
-        onClick={this.onSelectGem}></div>
+        onClick={this.onSelectGem}>
+        <div className={styles['game-controls']}>
+          <button
+            className={styles['button-fullscreen']}
+            onClick={this.toggleFullscreen}>
+            Переключить режим просмотра
+          </button>
+        </div>
+      </div>
     )
   }
 }

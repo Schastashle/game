c7ae72ab68edbd33a7e4eac87bbd89100bb488d0
mrdarkside <tomitomion@gmail.com>
2023-07-23 16:19:57 +0300

    :adhesive_bandage: fix exports

--- packages/client/src/components/index.ts
+++ packages/client/src/components/index.ts
@@ -1,3 +1,3 @@
-export { LinkItem, Button, Input } from './UI'
+export { LinkItem, Button, Input, Avatar } from './UI'
 export { default as AuthForm } from './AuthForm'
 export { default as ErrorBoundary } from './ErrorBoundary'

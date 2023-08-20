4b1daf9974f6575154f99e9276974ac25ac02d96
mrdarkside <tomitomion@gmail.com>
2023-08-20 19:24:01 +0300

    :hammer: добавляет скрипты

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
+// import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
 dotenv.config()
@@ -12,15 +12,18 @@ export default defineConfig({
   define: {
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
+  build: {
+    target: 'esnext',
+  },
   plugins: [
     react(),
-    VitePWA({
-      strategies: 'injectManifest',
-      srcDir: 'src',
-      filename: 'sw.js',
-      devOptions: {
-        enabled: false,
-      },
-    }),
+    // VitePWA({
+    //   strategies: 'injectManifest',
+    //   srcDir: 'src',
+    //   filename: 'sw.js',
+    //   devOptions: {
+    //     enabled: false,
+    //   },
+    // }),
   ],
 })

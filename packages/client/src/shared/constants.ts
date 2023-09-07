1ac605d6ffa6758aa63a154cffeebdbb7428ec80
mrdarkside <tomitomion@gmail.com>
2023-09-07 18:18:21 +0300

    хардкод порта сервера

--- packages/client/src/shared/constants.ts
+++ packages/client/src/shared/constants.ts
@@ -2,7 +2,7 @@
 // export const API_ROOT = `${BASE_URL}/api/v2`
 
 // редирект на локальный сервер с которого идут все запросы на API
-const __SERVER_PORT__ = import.meta.env.VITE_SERVER_PORT || 3000
+const __SERVER_PORT__ = 3000
 export const SERVER_URL = `http://localhost:${__SERVER_PORT__}`
 
 export const API_ROOT = `${SERVER_URL}/api/v2`

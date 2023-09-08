69fc81282287a689ad6cc0bca2baf17867553641
mrdarkside <tomitomion@gmail.com>
2023-09-08 21:52:48 +0300

    test

--- packages/client/src/shared/constants.ts
+++ packages/client/src/shared/constants.ts
@@ -1,8 +1,5 @@
-import dotenv from 'dotenv'
-
-dotenv.config()
-
 // редирект на локальный сервер с которого идут все запросы на API
-export const SERVER_URL = process.env.SERVER_URL || `http://localhost:3000`
+export const SERVER_URL =
+  import.meta.env.VITE_SERVER_URL || `http://localhost:3000`
 
 export const API_ROOT = `${SERVER_URL}/api/v2`

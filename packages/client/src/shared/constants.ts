e503dd1d4f75bd11c00c7ed7aa40d0a75888cc88
mrdarkside <tomitomion@gmail.com>
2023-09-08 20:37:15 +0300

    test

--- packages/client/src/shared/constants.ts
+++ packages/client/src/shared/constants.ts
@@ -2,6 +2,6 @@
 // export const API_ROOT = `${BASE_URL}/api/v2`
 
 // редирект на локальный сервер с которого идут все запросы на API
-export const SERVER_URL = `http://fiar.ya-praktikum.tech/`
+export const SERVER_URL = `http://fiar.ya-praktikum.tech:3000`
 
 export const API_ROOT = `${SERVER_URL}/api/v2`

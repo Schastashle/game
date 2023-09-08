c7dd8e69d21b52fdc6110a3736a15c19d06549f6
mrdarkside <tomitomion@gmail.com>
2023-09-09 00:31:01 +0300

    test

--- packages/client/src/shared/constants.ts
+++ packages/client/src/shared/constants.ts
@@ -1,5 +1,4 @@
 // редирект на локальный сервер с которого идут все запросы на API
-export const SERVER_URL =
-  import.meta.env.VITE_SERVER_URL || `http://localhost:3000`
+export const SERVER_URL = import.meta.env.VITE_SERVER_URL || `http://nginx:3000`
 
 export const API_ROOT = `${SERVER_URL}/api/v2`

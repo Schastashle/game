608dfc4ee2503479861b703f64f55b4c34ab7345
mrdarkside <tomitomion@gmail.com>
2023-09-08 21:27:47 +0300

    test

--- packages/client/src/shared/constants.ts
+++ packages/client/src/shared/constants.ts
@@ -1,7 +1,8 @@
-// const BASE_URL = 'https://ya-praktikum.tech'
-// export const API_ROOT = `${BASE_URL}/api/v2`
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 // редирект на локальный сервер с которого идут все запросы на API
-export const SERVER_URL = `http://fiar.ya-praktikum.tech:3000`
+export const SERVER_URL = process.env.SERVER_URL || `http://localhost:3000`
 
 export const API_ROOT = `${SERVER_URL}/api/v2`

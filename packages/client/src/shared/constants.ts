3ac81b43d5d43c5f6b73d558c77392633a5cdac3
mrdarkside <tomitomion@gmail.com>
2023-09-09 02:06:11 +0300

    test

--- packages/client/src/shared/constants.ts
+++ packages/client/src/shared/constants.ts
@@ -1,5 +1,5 @@
 // редирект на локальный сервер с которого идут все запросы на API
 export const SERVER_URL =
-  import.meta.env.VITE_SERVER_URL || 'http://host.docker.internal:3000'
+  import.meta.env.VITE_SERVER_URL || 'http://172.17.0.1:3000'
 
 export const API_ROOT = `${SERVER_URL}/api/v2`
